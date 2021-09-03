05bc44db6a27bc6e2c99e34a8a64f426ad3297cd
lili <lili03@rd.netease.com>
2021-09-03 17:54:39 +0800

    up

---
+++ src/test/java/org/lili/validator/ValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lili.validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
